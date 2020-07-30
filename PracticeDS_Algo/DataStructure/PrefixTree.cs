4b3b70145fb7b39323a14bac5699dcff6afc4885
ashiqqa <ashiq.mail.com@gnail.com>
2020-07-30 18:50:19 +0600

    prefix/Radix Tree (Trie) - insert and search string

---
+++ PracticeDS_Algo/DataStructure/PrefixTree.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeDS_Algo.DataStructure
+{
+    //Trie
+    public class PrefixTree
+    {
+        public PrefixTree()
+        {
+            Root = new Node();
+        }
+        private Node Root { get; set; }
+
+        /// <summary>
+        /// TimeComplexity O(length)
+        /// </summary>
+        /// <param name="word"></param>
+        public void Insert(string word)
+        {
+            word = word.ToLower();
+            Node temp = Root;
+            foreach (var ch in word)
+            {
+                int ch_idx = ch - 'a';
+                if (temp.Next[ch_idx] == null)
+                {
+                    temp.Next[ch_idx] = new Node();
+                }
+                temp = temp.Next[ch_idx];
+            }
+            temp.EndMark = true;
+        }
+
+        /// <summary>
+        /// TimeComplexity O(length)
+        /// </summary>
+        /// <param name="word"></param>
+        public bool Contains(string word)
+        {
+            Node temp = Root;
+            word = word.ToLower();
+            foreach (var ch in word)
+            {
+                int ch_idx = ch - 'a';
+                if(temp.Next[ch_idx] is null)
+                {
+                    return false;
+                }
+                temp = temp.Next[ch_idx];
+            }
+            return temp.EndMark;
+        }
+    }
+
+    class Node
+    {
+        public Node()
+        {
+            Next = new Node[26 + 1];
+            for(int i=0; i<26; i++)
+            {
+                Next[i] = null;
+            }
+        }
+        public bool EndMark { get; set; }
+        public Node[] Next { get; set; }
+    }
+}
--- PracticeDS_Algo/Program.cs
+++ PracticeDS_Algo/Program.cs
@@ -8,17 +8,15 @@ namespace PracticeDS_Algo
 {
     class Program
     {
-        static void Main(string[] args)
+        static unsafe void Main(string[] args)
         {
-            DoublyLinkedList<int> list = new DoublyLinkedList<int>();
-            var list0=list.Postpend(0);
-            list.Postpend(5);
-            var list2 = list.Postpend(10);
-            list.Postpend(15);
-            list.Postpend(20);
-            var list5 = list.Postpend(25);
-            list5.Next = list2;
-            var cycleNode = list.HasCycle();
+            PrefixTree trie = new PrefixTree();
+            trie.Insert("ball");
+            trie.Insert("bbc");
+            trie.Insert("bat");
+            Console.WriteLine(trie.Contains("bar"));
+            Console.WriteLine(trie.Contains("ball"));
+
         }
 
     }
